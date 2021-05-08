24bc63ddd1dbcb7a07714f11e0788d468ff986ca
Pholish <rogue.pholish@gmail.com>
2021-05-08 19:29:11 +0300

    New: auth logic and bcrypt security

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,98 @@
+const bcryptjs = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+
+  if (message.length) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    error: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+
+  if (message.length) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
+    error: message,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
+  const { email, password } = req.body;
+
+  User.findOne({ email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(
-          "🚀 ~ file: auth.js ~ line 25 ~ req.session.save ~ err",
-          err
-        );
-        res.redirect("/");
-      });
+      if (!user) {
+        req.flash("error", "Invalid email");
+        return res.redirect("/login");
+      }
+      bcryptjs
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          req.flash("error", "Invalid password");
+          res.redirect("/login");
+        })
+        .catch(() => res.redirect("/login"));
     })
     .catch((err) => {
       console.log("🚀 ~ file: auth.js ~ line 30 ~ err", err);
     });
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const { email, password, confirmPassword } = req.body;
+
+  User.findOne({ email })
+    .then((userDoc) => {
+      console.log("🚀 ~ file: auth.js ~ line 42 ~ .then ~ userDoc", userDoc);
+      if (userDoc) {
+        req.flash("error", "Email already exist");
+        return res.redirect("/signup");
+      }
+
+      return bcryptjs
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
+    })
+
+    .catch(console.log);
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
