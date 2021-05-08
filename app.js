d8e220c5fe0ae94c6b9716bd0071266a1d40dfd1
Rakheb Vataniar <admin@Rakhebs-MacBook-Pro.local>
2021-05-08 18:52:16 +0300

    New: auth logic and bcrypt security

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -16,6 +17,7 @@ const store = new MongoDBStore({
   uri: process.env.NODE_MONGO_DB,
   collection: "sessions",
 });
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -34,6 +36,7 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -49,6 +52,13 @@ app.use((req, res, next) => {
     });
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+
+  next();
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -58,19 +68,6 @@ app.use(errorController.get404);
 mongoose
   .connect(process.env.NODE_MONGO_DB)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Rakheb",
-          email: "rogue.pholish@gmail.com",
-          cart: {
-            items: [],
-          },
-        });
-
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(console.log);
